7cc45c194312e07a2cc80335cbdaee9bf064f04b
Brian <brianvicthorandp@gmail.com>
2021-05-05 20:22:38 -0300

    :joy: Adding a new AMV card and using destructuring

--- script.js
+++ script.js
@@ -22,18 +22,24 @@ const videos = [
     time: "4min 30",
     image: "./images/boku-no-hero.jpg"
     },
+    {
+        title: "Ippo Vs Sawamura - One of Us is Going DOWN" ,
+        url: "https://www.youtube.com/watch?v=0BRGUB3lqdE",
+        time: "4min 20",
+        image: "./images/hajime-no-ippo.jpg"
+    }
 ]
 
 const showVideos = ()=> {
-    videos.forEach( (video) => {
+    videos.forEach( ({title, url, time, image}) => {
         let additionalCard = cardTemplate.cloneNode(true);
         additionalCard.querySelectorAll('a').forEach( link => {
-            link.href = video.url
+            link.href = url
         }) 
-        additionalCard.querySelector('img').src = video.image
-        additionalCard.querySelector('img').alt = video.title
-        additionalCard.querySelector('h3').textContent = video.title
-        additionalCard.querySelector('small').textContent = video.time
+        additionalCard.querySelector('img').src = image
+        additionalCard.querySelector('img').alt = title
+        additionalCard.querySelector('h3').textContent = title
+        additionalCard.querySelector('small').textContent = time
         videosContainer.appendChild(additionalCard)
     }) 
 }
